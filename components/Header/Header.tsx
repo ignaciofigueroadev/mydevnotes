5ea7fb494138302771be2269e83633029b11a914
Ignacio Figueroa <121741994+ignaciofigueroadev@users.noreply.github.com>
2024-01-09 17:36:19 -0300

    fix: removing unnecessary buttons

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -8,7 +8,6 @@ import { MobileMenu } from "../MobileMenu/MobileMenu";
 import { ToggleThemeButton } from "../ToggleThemeButton/ToggleThemeButton";
 
 // shadcn/ui components
-import { Button } from "../ui/button";
 
 // Next imports
 import Link from "next/link";
@@ -29,48 +28,36 @@ export function Header() {
         <nav className="hidden lg:block">
           <ul className="flex flex-col gap-6 w-screen items-center md:w-full md:flex-row md:min-h-0 border rounded-full px-2 py-2">
             <li>
-              <Link href="/" className="cursor-default flex items-center gap-1">
-                <Button
-                  variant="ghost"
-                  className={`flex gap-2 hover:bg-indigo-400 hover:text-black ${
-                    pathname === "/" ? "bg-indigo-400 text-black" : ""
-                  }`}
-                >
-                  <HomeIcon size=".75rem" />
-                  Home
-                </Button>
+              <Link
+                href="/"
+                className={`flex gap-2 hover:bg-indigo-400 hover:text-black py-3 px-4 rounded-full justify-center items-center cursor-default ${
+                  pathname === "/" ? "bg-indigo-400 text-black" : ""
+                }`}
+              >
+                <HomeIcon size=".75rem" />
+                Home
               </Link>
             </li>
             <li>
               <Link
                 href="/posts"
-                className="cursor-default flex items-center gap-1"
+                className={`flex gap-2 hover:bg-fuchsia-600 hover:text-black py-3 px-4 rounded-full justify-center items-center cursor-default ${
+                  pathname === "/posts" ? "bg-fuchsia-600 text-black" : ""
+                }`}
               >
-                <Button
-                  variant="ghost"
-                  className={`flex gap-2 hover:bg-fuchsia-600 hover:text-black ${
-                    pathname === "/posts" ? "bg-fuchsia-600 text-black" : ""
-                  }`}
-                >
-                  <MessageSquare size=".75rem" />
-                  Posts
-                </Button>
+                <MessageSquare size=".75rem" />
+                Posts
               </Link>
             </li>
             <li>
               <Link
                 href="/all-articles"
-                className="cursor-default flex items-center gap-3"
+                className={`flex gap-1 hover:bg-pink-400 hover:text-black py-3 px-4 rounded-full justify-center items-center cursor-default ${
+                  pathname === "/all-articles" ? "bg-pink-400 text-black" : ""
+                }`}
               >
-                <Button
-                  variant="ghost"
-                  className={`flex gap-1 hover:bg-pink-400 hover:text-black ${
-                    pathname === "/all-articles" ? "bg-pink-400 text-black" : ""
-                  }`}
-                >
-                  <FileText size=".75rem" />
-                  Articles
-                </Button>
+                <FileText size=".75rem" />
+                Articles
               </Link>
             </li>
           </ul>
