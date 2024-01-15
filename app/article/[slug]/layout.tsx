238c851be8d142537e044c034265c9e14ab34ad4
Ignacio Figueroa <121741994+ignaciofigueroadev@users.noreply.github.com>
2024-01-15 15:54:59 -0300

    feat: adding test description to the article layout

--- app/article/[slug]/layout.tsx
+++ app/article/[slug]/layout.tsx
@@ -6,7 +6,7 @@ import type { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "My Dev Notes",
-  description: "",
+  description: "This is an article",
 };
 
 export default function ArticleDetailsLayout({
