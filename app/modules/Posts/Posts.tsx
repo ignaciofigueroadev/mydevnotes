ed8873c50cdb257ba1be46cfcc3e3b4d0a8c184d
Ignacio Figueroa <121741994+ignaciofigueroadev@users.noreply.github.com>
2024-01-16 12:42:48 -0300

    feat: adding skeleton post card to the posts page

--- app/modules/Posts/Posts.tsx
+++ app/modules/Posts/Posts.tsx
@@ -4,7 +4,7 @@
 import { Header } from "@/components/Header/Header";
 import { PostCard } from "@/components/PostCard/PostCard";
 import { ScrollDown } from "@/components/ScrollDown/ScrollDown";
-import { Spinner } from "@/components/Spinner/Spinner";
+import { SkeletonPostCard } from "@/components/SkeletonPostCard/SkeletonPostCard";
 import { Separator } from "@/components/ui/separator";
 
 // Get posts
@@ -17,10 +17,30 @@ export default function Posts() {
 
   if (isLoading) {
     return (
-      <div>
+      <>
         <Header />
-        <Spinner className="flex justify-center items-center min-h-screen" />
-      </div>
+        <section className="py-10">
+          <div className=" flex flex-col gap-5">
+            <div className="flex flex-col gap-3 justify-center items-center min-h-[40vh] text-center">
+              <h3 className="font-bold text-6xl lg:text-7xl bg-gradient-to-r from-indigo-500 via-fuchsia-700 to-violet-400 inline-block text-transparent bg-clip-text">
+                Posts
+              </h3>
+              <p>
+                In this section you are going to find quick posts as tips and
+                advices.
+              </p>
+              <ScrollDown />
+            </div>
+            <Separator />
+            <div className="grid gap-10 grid-cols-1 justify-center items-center pt-10">
+              <SkeletonPostCard />
+              <SkeletonPostCard />
+              <SkeletonPostCard />
+              <SkeletonPostCard />
+            </div>
+          </div>
+        </section>
+      </>
     );
   }
 
