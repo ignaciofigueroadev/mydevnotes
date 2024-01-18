14c1d1186bf095e1317b9c91d02a89490793a1e2
Ignacio Figueroa <121741994+ignaciofigueroadev@users.noreply.github.com>
2024-01-18 12:03:52 -0300

    feat: adding description to the home page

--- app/modules/Home/Home.tsx
+++ app/modules/Home/Home.tsx
@@ -13,11 +13,21 @@ export function Home() {
         <h1 className="font-bold text-6xl lg:text-7xl bg-gradient-to-r from-indigo-500 via-fuchsia-700 to-violet-400 inline-block text-pretty text-transparent bg-clip-text">
           My Dev Notes
         </h1>
-        <div className="flex gap-8">
+        <p>
+          A blog build by a{" "}
+          <span className="bg-gradient-to-r from-indigo-500 via-fuchsia-700 to-violet-400 inline-block text-pretty text-transparent bg-clip-text">
+            developer
+          </span>{" "}
+          for{" "}
+          <span className="bg-gradient-to-r from-indigo-500 via-fuchsia-700 to-violet-400 inline-block text-pretty text-transparent bg-clip-text">
+            developers
+          </span>
+        </p>
+        <div className="flex  gap-8">
           <Link href="/about">
             <Button
               variant="outline"
-              className="font-bold hover:-translate-y-2 transition-all duration-150"
+              className="font-bold hover:-translate-y-1 transition-all duration-150"
             >
               About
             </Button>
@@ -25,7 +35,7 @@ export function Home() {
           <Link href="#latest-articles">
             <Button
               variant="outline"
-              className="font-bold hover:-translate-y-2 transition-all duration-150"
+              className="font-bold hover:-translate-y-1 transition-all duration-150"
             >
               Explore
             </Button>
