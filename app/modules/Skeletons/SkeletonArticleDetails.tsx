5246795fba2fd689df39d6f3f3729a01a7d06969
Ignacio Figueroa <figueroaignaciodev@gmail.com>
2024-02-02 17:58:46 -0300

    fix: fixing the bg color

--- app/modules/Skeletons/SkeletonArticleDetails.tsx
+++ app/modules/Skeletons/SkeletonArticleDetails.tsx
@@ -1,34 +1,43 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
 export default function SkeletonArticleDetails() {
+  const skeletonWidths = [
+    "1/2",
+    "3/4",
+    "2/3",
+    "full",
+    "5/6",
+    "1/3",
+    "2/5",
+    "3/6",
+    "4/6",
+    "1/2",
+    "2/4",
+    "3/5",
+    "4/7",
+    "5/8",
+    "6/9",
+    "7/10",
+    "8/11",
+    "9/12",
+    "10/13",
+    "11/14",
+    "12/15",
+    "13/16",
+    "14/17",
+    "15/18",
+    "16/19",
+    "17/20",
+  ];
+
   return (
     <div className="flex flex-col gap-4 pt-12 ">
-      <Skeleton className="h-4 w-1/2" />
-      <Skeleton className="h-4 w-3/4" />
-      <Skeleton className="h-4 w-2/3" />
-      <Skeleton className="h-4 w-full" />
-      <Skeleton className="h-4 w-5/6" />
-      <Skeleton className="h-4 w-1/3" />
-      <Skeleton className="h-4 w-2/5" />
-      <Skeleton className="h-4 w-3/6" />
-      <Skeleton className="h-4 w-4/6" />
-      <Skeleton className="h-4 w-1/2" />
-      <Skeleton className="h-4 w-2/4" />
-      <Skeleton className="h-4 w-3/5" />
-      <Skeleton className="h-4 w-4/7" />
-      <Skeleton className="h-4 w-5/8" />
-      <Skeleton className="h-4 w-6/9" />
-      <Skeleton className="h-4 w-7/10" />
-      <Skeleton className="h-4 w-8/11" />
-      <Skeleton className="h-4 w-9/12" />
-      <Skeleton className="h-4 w-10/13" />
-      <Skeleton className="h-4 w-11/14" />
-      <Skeleton className="h-4 w-12/15" />
-      <Skeleton className="h-4 w-13/16" />
-      <Skeleton className="h-4 w-14/17" />
-      <Skeleton className="h-4 w-15/18" />
-      <Skeleton className="h-4 w-16/19" />
-      <Skeleton className="h-4 w-17/20" />
+      {skeletonWidths.map((width, index) => (
+        <Skeleton
+          key={index}
+          className={`h-4 w-${width} bg-gray-300 dark:bg-gray-700`}
+        />
+      ))}
     </div>
   );
 }
