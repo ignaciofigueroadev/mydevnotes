48808561f2bdeec4088da60dd0f6e8a992b33e88
Ignacio Figueroa <figueroaignaciodev@gmail.com>
2024-02-07 23:30:39 -0300

    refactor: adding new skeleton loaders

--- app/modules/Posts.tsx
+++ app/modules/Posts.tsx
@@ -5,7 +5,7 @@ import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import { PostCard } from "@/components/PostCard";
 import { ScrollDown } from "@/components/ScrollDown";
-import { SkeletonPostCard } from "@/components/SkeletonPostCard";
+import SkeletonAllPosts from "./Skeletons/SkeletonAllPosts";
 
 // Get posts
 import { usePosts } from "@/hooks/usePosts";
@@ -20,36 +20,7 @@ export default function Posts() {
   const { isError, isLoading, posts } = usePosts();
 
   if (isLoading) {
-    return (
-      <>
-        <Header />
-        <section className="py-10">
-          <div className=" flex flex-col gap-5">
-            <div className="flex flex-col gap-8 justify-center items-center min-h-[60vh] text-center">
-              <div>
-                <h3 className="font-bold text-5xl lg:text-7xl bg-gradient-to-r from-indigo-500 via-fuchsia-700 to-violet-400 inline-block text-transparent bg-clip-text">
-                  Posts
-                </h3>
-                <p className="text-xs lg:text-lg">
-                  In this section you are going to find quick posts as tips and
-                  advices.
-                </p>
-              </div>
-              <Link href="#posts">
-                <ScrollDown />
-              </Link>
-            </div>
-            <div className="grid gap-10 grid-cols-1 justify-center items-center pt-10">
-              <SkeletonPostCard />
-              <SkeletonPostCard />
-              <SkeletonPostCard />
-              <SkeletonPostCard />
-            </div>
-          </div>
-        </section>
-        <Footer />
-      </>
-    );
+    return <SkeletonAllPosts />;
   }
 
   if (isError) {
---
+++ app/modules/Skeletons/SkeletonAllArticles.tsx
@@ -0,0 +1,36 @@
+// Custom components
+import { ArticleSearch } from "@/components/ArticleSearch";
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+import { ScrollDown } from "@/components/ScrollDown";
+import { SkeletonArticleCard } from "@/components/SkeletonArticleCard";
+
+export default function SkeletonAllArticles() {
+  return (
+    <>
+      <Header />
+      <div className="flex flex-col gap-2 py-10 ">
+        <div className="flex flex-col gap-3 justify-center items-center text-center min-h-[60vh]">
+          <h3 className="font-bold text-5xl lg:text-7xl bg-gradient-to-r from-indigo-500 via-fuchsia-700 to-violet-400 inline-block text-transparent bg-clip-text">
+            Articles
+          </h3>
+          <p className="text-xs lg:text-lg">
+            In this section you are going to find articles about tech and
+            coding. Check them!
+          </p>
+          <ScrollDown />
+        </div>
+        <div className="flex">
+          <ArticleSearch />
+        </div>
+        <section className="grid grid-cols-1 lg:grid-cols-2 gap-7">
+          <SkeletonArticleCard />
+          <SkeletonArticleCard />
+          <SkeletonArticleCard />
+          <SkeletonArticleCard />
+        </section>
+      </div>
+      <Footer />
+    </>
+  );
+}
