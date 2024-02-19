b583f6278e9b0d4a38d10ad59523ab0f00031005
Ignacio Figueroa <figueroaignaciodev@gmail.com>
2024-02-19 16:35:45 -0300

    style: changing the logo styles

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,12 +3,12 @@ import Link from "next/link";
 export function Logo() {
   return (
     <Link href="/">
-      <div className="w-12 h-22 flex flex-col justify-center rounded-md gap-1">
-        <div className="h-2 w-8 bg-yellow-400 rounded"></div>
-        <div className="h-2 w-14 bg-green-400 rounded"></div>
-        <div className="h-2 w-7 bg-blue-400 rounded"></div>
-        <div className="h-2 w-9 bg-indigo-400 rounded"></div>
-        <div className="h-2 w-14 bg-pink-400 rounded"></div>
+      <div className="flex flex-col justify-center rounded-md gap-1">
+        <div className="h-1.5 w-8 bg-yellow-400 rounded"></div>
+        <div className="h-1.5 w-14 bg-green-400 rounded"></div>
+        <div className="h-1.5 w-7 bg-blue-400 rounded"></div>
+        <div className="h-1.5 w-9 bg-indigo-400 rounded"></div>
+        <div className="h-1.5 w-14 bg-pink-400 rounded"></div>
       </div>
     </Link>
   );
