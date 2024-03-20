d2c87e6005bd57c31fb91719c420dfecfd746825
Ignacio Figueroa <figueroaignaciodev@gmail.com>
2024-03-20 17:37:10 -0300

    fix: removing icon in the mobile menu

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -6,7 +6,7 @@ import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { ToggleThemeButton } from "./ToggleThemeButton";
 
 // Icons
-import { HomeIcon, Menu, MessageSquare, SheetIcon } from "lucide-react";
+import { Menu } from "lucide-react";
 
 // Next
 import { navItems } from "@/constants/site";
@@ -26,20 +26,6 @@ export function MobileMenu() {
       <SheetContent side="left">
         <ul className="flex flex-col gap-7 pt-32 py-10 pl-8 items-end md:w-full md:min-h-0 text-2xl">
           {navItems.map((navItem, index) => {
-            let IconComponent;
-            switch (navItem.icon) {
-              case "home":
-                IconComponent = <HomeIcon size=".75rem" />;
-                break;
-              case "post":
-                IconComponent = <MessageSquare size=".75rem" />;
-                break;
-              case "article":
-                IconComponent = <SheetIcon size=".75rem" />;
-                break;
-              default:
-                IconComponent = null;
-            }
             return (
               <li key={index}>
                 <Button asChild variant="navItem" title={navItem.title}>
@@ -51,7 +37,6 @@ export function MobileMenu() {
                         : ""
                     }`}
                   >
-                    {IconComponent}
                     {navItem.title}
                   </Link>
                 </Button>
